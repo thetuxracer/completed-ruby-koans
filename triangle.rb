ebef488f89f5d8d44333b28a057908895dad0280
Kaustubh Padegaonkar <theTuxRacer@gmail.com>
2013-08-05 16:35:02 +0530

    recognizes an equilateral traingle

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	if a == b && b == c
+		return :equilateral
+	elsif false
+		return :isosceles
+	elsif false
+		return :scalene
+	end
   # WRITE THIS CODE
 end
 
